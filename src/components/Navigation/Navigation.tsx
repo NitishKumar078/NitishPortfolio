fc8beb1f00906302dfa6e1829a505b251cd558a1
nitish kumar <nishkumar562@gmail.com>
2024-12-06 23:28:27 +0530

    Fixed navigation alignment

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -64,12 +64,8 @@ export const Navigation = ({ navRefs }: NavigationProps) => {
 
   return (
     <header className="fixed left-0 right-0 top-0 z-50 border-b border-zinc-200 bg-white/80 backdrop-blur-md dark:border-zinc-800 dark:bg-zinc-900/80">
-      <div className="mx-auto flex h-16 max-w-7xl items-center justify-between px-4 sm:px-6 lg:px-8">
-        {/* Logo and ThemeToggle */}
-        <div className="flex items-center gap-4">
-          <Logo />
-          <ThemeToggle />
-        </div>
+      <div className="mx-auto flex h-16 items-center justify-between px-4 sm:px-6 lg:px-8">
+        <Logo />
 
         {/* Centered Navigation Links */}
         <nav
@@ -126,6 +122,7 @@ export const Navigation = ({ navRefs }: NavigationProps) => {
           onClose={() => setIsMobileMenuOpen(false)}
           navRefs={navRefs}
         />
+        <ThemeToggle />
       </div>
     </header>
   );
