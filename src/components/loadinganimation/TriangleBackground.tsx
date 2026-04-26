79e91c8796634ed5794eb5d696f3ab40a8e2197d
Nitish_kumar <nishkumar562@gmail.com>
2026-04-26 20:57:02 +0530

    deleted Everything for new-design

--- src/components/loadinganimation/TriangleBackground.tsx
+++
@@ -1,9 +0,0 @@
-export const TriangleBackground = () => {
-  return (
-    <>
-      {/* Dark Theme Triangle Background */}
-      <div className="absolute h-0 w-0 border-b-[400px] border-l-[200px] border-r-[200px] border-b-[#9234eb] border-l-transparent border-r-transparent animate-spin-slow" />
-      <div className="absolute h-0 w-0 border-b-[390px] border-l-[190px] border-r-[190px] border-white border-l-transparent border-r-transparent dark:border-[#19191c] dark:border-l-transparent dark:border-r-transparent animate-spin-reverse-slow" />
-    </>
-  );
-};
